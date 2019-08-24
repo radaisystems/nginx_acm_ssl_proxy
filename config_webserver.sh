b4014f51a7a78bf8ef2106ce57fcb235e339f70b
mfergus1 <mia@radai.com>
2019-08-23 20:58:28 -0700

    Always return just one arn. ACM filters are not good

--- config_webserver.sh
+++ config_webserver.sh
@@ -17,7 +17,7 @@ elif [[ -z "$FQDN" ]]; then
 fi
 
 secret_re='.*"SecretString": "(.*)",.*'
-cert_arn=$(eval "aws acm list-certificates --query 'CertificateSummaryList[?DomainName==\`${FQDN}\`]'.[CertificateArn] --output text")
+cert_arn=$(eval "aws acm list-certificates --query 'CertificateSummaryList[?DomainName==\`${FQDN}\`]'.[CertificateArn] --output text --max-items 1")
 
 if [[ $(aws secretsmanager get-secret-value --secret-id ${FQDN}) =~ $secret_re ]]; then
   cert_pass_phrase=${BASH_REMATCH[1]}
