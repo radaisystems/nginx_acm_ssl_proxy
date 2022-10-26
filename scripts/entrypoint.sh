c41fcc93601b4700046469780bcee56ae487627b
Kyle T <kylet21@gmail.com>
2022-10-25 20:38:36 -0400

    add Nginx rate limiting

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -28,6 +28,58 @@ if [[ -z "$SERVER_NAME" ]]; then
   export SERVER_NAME
 fi
 
+# ENABLE_RATE_LIMITING enables (uncomments) the rate limiting section of the config file.
+if [[ "$(echo $ENABLE_RATE_LIMITING | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+  RATE_LIMIT_LINE_PREFIX=""
+  export RATE_LIMIT_LINE_PREFIX
+else
+  RATE_LIMIT_LINE_PREFIX="# "
+  export RATE_LIMIT_LINE_PREFIX
+fi
+
+# REAL_IP_ALLOWED_CIDR will default to our standard AWS VPC CIDR.
+if [[ -z "$REAL_IP_ALLOWED_CIDR" ]]; then
+  REAL_IP_ALLOWED_CIDR="0.0.0.0/0"
+  export REAL_IP_ALLOWED_CIDR
+fi
+
+# RATE_LIMIT_STATE_SIZE dictates how many IP state statuses are stored in memory (in MB). 1 = ~ 16,000 IPs
+if [[ -z "$RATE_LIMIT_STATE_SIZE" ]]; then
+  RATE_LIMIT_STATE_SIZE="1"
+  export RATE_LIMIT_STATE_SIZE
+fi
+
+# RATE_LIMIT_REQUESTS dictates how many requests per second are allowed.
+if [[ -z "$RATE_LIMIT_REQUESTS" ]]; then
+  RATE_LIMIT_REQUESTS="256"
+  export RATE_LIMIT_REQUESTS
+fi
+
+# RATE_LIMIT_BURST_SIZE number of requests allowed to be sent to the burst queue.
+if [[ -z "$RATE_LIMIT_BURST_SIZE" || "$RATE_LIMIT_BURST_SIZE" == "0" ]]; then
+  # RATE_LIMIT_BURST_SIZE="10"
+  # export RATE_LIMIT_BURST_SIZE
+  RATE_LIMIT_BURST=""
+  export RATE_LIMIT_BURST
+else
+  # RATE_LIMIT_BURST_NODELAY send requests from burst queue immediately with no delay.
+  if [[ "$(echo $RATE_LIMIT_BURST_NODELAY | tr '[:upper:]' '[:lower:]')" == "false" ]]; then
+    RATE_LIMIT_BURST=" burst=$RATE_LIMIT_BURST_SIZE"
+  else
+    RATE_LIMIT_BURST=" burst=$RATE_LIMIT_BURST_SIZE nodelay"
+  fi
+  export RATE_LIMIT_BURST
+fi
+
+# RATE_LIMIT_NODELAY send requests from burst queue immediately with no delay.
+if [[ "$(echo $RATE_LIMIT_NODELAY | tr '[:upper:]' '[:lower:]')" == "false" ]]; then
+  RATE_LIMIT_NODELAY=""
+  export RATE_LIMIT_NODELAY
+else
+  RATE_LIMIT_NODELAY=" nodelay"
+  export RATE_LIMIT_NODELAY
+fi
+
 if [ -n "$DEBUG" ]; then
     echo "Environment variables:"
     env
